a62907eb372a896b6f677af21842c6a2db41e474
KemiBrown <durosinmioluwakemi@gmail.com>
2023-02-19 18:18:01 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: a program that prints all single digit numbers of base 10
+ * starting from 0
+ * Return: 0
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
