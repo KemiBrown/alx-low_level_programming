db5e55a4922be29364e2b511115d4ef4c5edbe17
KemiBrown <durosinmioluwakemi@gmail.com>
2023-03-10 10:06:00 -0800

    3-puts edited

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,9 @@
 #include "main.h"
+void _puts(char *str)
 #include<stdio.h>
-
 /**
  * main - Entry point
- * _puts: prints out whatever main function wants
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,3 +11,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
