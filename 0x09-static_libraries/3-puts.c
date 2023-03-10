462c9cc31e57f81c06782f44c5efd14fe6222a25
KemiBrown <durosinmioluwakemi@gmail.com>
2023-03-10 09:13:51 -0800

    my prototypes, functions copied into my libmy.a library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * _puts: prints out whatever main function wants
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
